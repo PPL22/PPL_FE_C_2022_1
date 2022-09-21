fc23b331bddd5e2d35de5c7b87e2da809e760291
alvintriseptia <alvintryseptia@gmail.com>
2022-09-21 12:38:56 +0700

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
+import React from 'react';
+import axios from 'axios';
+import config from './configs/config.json';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useAuth } from './contexts/AuthContext.js';
 import { Home, Login } from './pages/pages.js';
 
 function App() {
+  const auth = useAuth();
+  const [loading, setLoading] = React.useState(true);
+
+  // verify token
+  // React.useEffect(() => {
+  //   const verifyToken = async () => {
+  //     try {
+  //       const tokenUser = localStorage.getItem('token');
+  //       if (tokenUser) {
+  //         await axios.get(`${config.API_URL}/verify`, {
+  //           headers: {
+  //             'x-access-token': tokenUser,
+  //           },
+  //         });
+  //         auth.setToken(tokenUser);
+  //       }
+  //     } catch (error) {
+  //       if (error.response.status === 401) {
+  //         auth.logout();
+  //       }
+  //       throw error;
+  //     }
+  //   };
+  //   verifyToken();
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 500);
+  // }, [auth, auth.token]);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,12 +1,52 @@
 import React from 'react';
+import axios from 'axios';
+import config from '../configs/config.json';
 import pic from '../assets/images/logo_undip.png';
 import { FaEnvelope, FaKey } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
 
 const Login = () => {
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+  // const { login } = useAuth();
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    setLoading(true);
+    setError(null);
+    try {
+      const url = `${config.API_URL}/login`;
+      const response = await axios.post(url, data);
+      if (response) {
+        const data = response.data;
+        //set storage
+        localStorage.setItem('accessToken', data.accessToken);
+        localStorage.setItem('refreshToken', data.refreshToken);
+        localStorage.setItem('username', data.username);
+        localStorage.setItem('role', data.role);
+        window.location.href = '/dashboard';
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error.response.data.message);
+      setError(error.response.data.message);
+      setLoading(false);
+      throw error;
+    }
+  };
+
   return (
     <section className="h-screen grid grid-cols-12">
-      <div className="hidden lg:block lg:col-span-7 bg-acintya-prasada bg-no-repeat bg-cover"></div>
-      <div className="col-span-12 lg:col-span-5 bg-background flex justify-center items-center">
+      <div className="hidden md:block md:col-span-7 bg-acintya-prasada bg-no-repeat bg-cover"></div>
+      <div className="col-span-12 md:col-span-5 bg-background flex justify-center items-center">
         <div className="">
           {/* header */}
           <div className="flex justify-center">
@@ -17,28 +57,32 @@ const Login = () => {
             Login to your account
           </p>
           {/* form */}
-          <form className="flex flex-col w-full space-y-6">
+          <form
+            className="flex flex-col w-full space-y-6"
+            onSubmit={handleSubmit(onSubmit)}
+          >
             <label
-              for="email-user"
-              class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+              htmlFor="username"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
             >
-              Email
+              Username
             </label>
             <div className="relative">
               <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                 <FaEnvelope color=" #808080" />
               </div>
               <input
-                type="email"
-                id="email-user"
+                type="text"
+                id="username"
                 className="block p-4 pl-10 w-full text-sm text-gray-900 bg-blue-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
-                placeholder="Email Address"
+                placeholder="Username"
                 required
+                {...register('username', { required: true })}
               />
             </div>
             <label
-              for="password-user"
-              class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+              htmlFor="password-user"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
             >
               Password
             </label>
@@ -52,6 +96,7 @@ const Login = () => {
                 className="block p-4 pl-10 w-full text-sm text-gray-900 bg-blue-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
                 placeholder="Password"
                 required
+                {...register('password', { required: true })}
               />
             </div>
             <div className="pt-4 flex justify-center">
