46df1541715fc442fa8d9c88c25dba00badfc4d4
alvintriseptia <alvintryseptia@gmail.com>
2022-10-26 11:39:27 +0700

    implement table akun mahasiswa

--- src/pages/pages.js
+++ src/pages/pages.js
@@ -1,18 +1,20 @@
-export { default as Home } from './home';
-export { default as Login } from './login';
-export { default as UpdateDataMhs } from './update_data_mhs';
-export { default as Dashboard } from './dashboard';
-export { default as Profile } from './profile';
-export { default as Dosen } from './dosen';
-export { default as Mahasiswa } from './mahasiswa';
-export { default as Table } from './table';
-export { default as DataMhs } from './data_mhs';
+export { default as Home } from "./home";
+export { default as Login } from "./login";
+export { default as UpdateDataMhs } from "./update_data_mhs";
+export { default as Dashboard } from "./dashboard";
+export { default as Profile } from "./profile";
+export { default as Dosen } from "./dosen";
+export { default as Mahasiswa } from "./mahasiswa";
+export { default as Table } from "./table";
+export { default as DataMhs } from "./data_mhs";
 
-export { default as StatusIRSMahasiswa } from './status_mahasiswa/irs/irs';
-export { default as StatusKHSMahasiswa } from './status_mahasiswa/khs/khs';
-export { default as StatusSkripsiMahasiswa } from './status_mahasiswa/skripsi/skripsi';
-export { default as StatusPKLMahasiswa } from './status_mahasiswa/pkl/pkl';
+export { default as StatusIRSMahasiswa } from "./status_mahasiswa/irs/irs";
+export { default as StatusKHSMahasiswa } from "./status_mahasiswa/khs/khs";
+export { default as StatusSkripsiMahasiswa } from "./status_mahasiswa/skripsi/skripsi";
+export { default as StatusPKLMahasiswa } from "./status_mahasiswa/pkl/pkl";
 
-export { default as RekapStatusMahasiswa } from './rekap_mahasiswa/status/status';
-export { default as RekapPKLMahasiswa } from './rekap_mahasiswa/pkl/pkl';
-export { default as RekapSkripsiMahasiswa } from './rekap_mahasiswa/skripsi/skripsi';
+export { default as RekapStatusMahasiswa } from "./rekap_mahasiswa/status/status";
+export { default as RekapPKLMahasiswa } from "./rekap_mahasiswa/pkl/pkl";
+export { default as RekapSkripsiMahasiswa } from "./rekap_mahasiswa/skripsi/skripsi";
+
+export { default as TableAkunMahasiswa } from "./akun_mahasiswa/table_akun_mahasiswa";
